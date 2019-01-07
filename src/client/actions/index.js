d698630967e4189772a4edc690a701c4e73694b4
Sergio Rocha <sergio@Sergios-MacBook-Pro.local>
2019-01-07 10:32:07 -0600

    Created a fetch users action and reducer

---
+++ src/client/actions/index.js
@@ -0,0 +1,16 @@
+import axios from 'axios';
+
+export const FETCH_USERS = 'fetch_users';
+
+const url = 'http://react-ssr-api.herokuapp.com/users';
+
+// Use redux-thunk's dispatch to dispatch the action once axios fetches 
+// the users
+export const fetchUsers = () => async dispatch => {
+    const response = await axios.get(url);
+    
+    dispatch({
+        type: FETCH_USERS,
+        payload: response
+    });
+};
